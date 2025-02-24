89fbeb2b4d75e1e5d45fa6774046300313be09f4
SimeonNedyalkov <simeon_sg@abv.bg>
2025-02-24 08:22:41 +0200

    user resource created

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as firebaseAdmin from 'firebase-admin';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const firebaseKeyFilePath =
+    './tehnotransport-7630b-firebase-adminsdk-fbsvc-dc961e67c4.json';
+  const firebaseServiceAccount = JSON.parse(
+    fs.readFileSync(firebaseKeyFilePath).toString(),
+  );
   const app = await NestFactory.create(AppModule);
+  if (firebaseAdmin.app.length === 0) {
+    console.log('Inititalise Firebase Application');
+    firebaseAdmin.initializeApp({
+      credential: firebaseAdmin.credential.cert(firebaseServiceAccount),
+    });
+  }
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
