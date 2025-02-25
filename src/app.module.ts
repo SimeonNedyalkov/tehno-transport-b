7682b92f1cdd422d9a6f59d3d5a445e99fa7c3d2
SimeonNedyalkov <simeon_sg@abv.bg>
2025-02-25 09:19:41 +0200

    customers created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthGuard } from './guards/auth.guard';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService, AuthGuard],
 })
