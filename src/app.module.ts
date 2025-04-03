501692970d422769b5e539fdbe27a110515a32eb
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-03 14:23:17 +0300

    due soon routes deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthGuard } from './guards/auth.guard';
 import { CustomersModule } from './customers/customers.module';
-import { DueSoonCustomersModule } from './due-soon-customers/due-soon-customers.module';
 import { MessageModule } from './message/message.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -13,7 +12,6 @@ import { join } from 'path';
   imports: [
     UserModule,
     CustomersModule,
-    DueSoonCustomersModule,
     MessageModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
--- src/due-soon-customers/dto/update-due-soon-customer.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDueSoonCustomerDto } from './create-due-soon-customer.dto';
-
-export class UpdateDueSoonCustomerDto extends PartialType(CreateDueSoonCustomerDto) {}
