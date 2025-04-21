2ba9e16833b45df584ba755b932e8ea6b7486c10
SimeonNedyalkov <simeon_sg@abv.bg>
2025-04-21 12:16:24 +0300

    secure = true

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
   });
   app.enableCors({
     origin: [
